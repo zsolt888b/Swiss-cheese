056cf05ca9f2f87355023fa41188e6e1018ff107
Berényi Zsolt <zsolt888b@gmail.com>
2020-11-28 16:11:23 +0100

    unauthorized exception handling

--- OnlineStore/Client/src/app/modules/interceptors/error.interceptor.ts
+++ OnlineStore/Client/src/app/modules/interceptors/error.interceptor.ts
@@ -9,17 +9,24 @@ import {
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
 import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-  constructor(private injector : Injector) {}
+  constructor(private injector : Injector, private toastr : ToastrService, private router : Router) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request)
       .pipe(
         retry(1),
         catchError((error: HttpErrorResponse) => {
+
+          if(error.status == 401){
+            this.toastr.error("Unauthorized!");
+            this.router.navigate(['']);
+            return throwError("Server side error");
+          }
 
           let self = this;
           let reader = new FileReader();
